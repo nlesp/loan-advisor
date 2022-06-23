12daaf718d1092c5bedbc7451bed4d35c3ca7720
nackcimelesperance <107566158+nackcimelesperance@users.noreply.github.com>
2022-06-23 21:29:02 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,99 +5,36 @@ Created on Mon Jun 13 16:39:04 2020
 @author:
 """
 # 1. Library imports
-import uvicorn
-from fastapi import FastAPI
-import numpy as np
-import pickle
+import requests
+import json
 import pandas as pd
-from processing_functions import add_variable, get_client
-from pydantic import BaseModel
+import streamlit as st
 
-#import sklearn
-#from lightgbm import LGBMClassifier
 # 2. Create the app object /  Initialize an instance of FastAPI
-app = FastAPI()
-
-
-pickle_in = open("pipeline_bank.pkl","rb")
-pipeline_process=pickle.load(pickle_in)
 
 data_train = pd.read_csv('application_train.csv')
+headers = {'Content-Type': 'application/json'}
+#url_id = "http://127.0.0.1:8000/predict"
+#url_list = "http://127.0.0.1:8000/list"
+url_id = "https://loan-advisor-api.herokuapp.com/predict"
+url_list = "https://loan-advisor-api.herokuapp.com/list"
+def main():
+    st.title("Bank Helper")
+    
+    #id_list = requests.post(url_list, headers=headers, json=payload)
+    #payload = st.selectbox('Choose Id client', id_list, help = 'Filter report to show only one id client')
+    payload = st.selectbox('Choose Id client', data_train['SK_ID_CURR'].tolist(), help = 'Filter report to show only one id client')
+    
+    pred = requests.post(url_id, headers=headers, json=payload)
 
-add_variable(data_train)
+    if st.button("Predict"):
+        result=predict_note(id)
+        st.success('The score of the client is {}'.format(pred))
+        #st.success("The score is "+str(round(prediction, 4))+" for the client "+str(id))
+        names='0', '1',
+        id_score = [1-pred,pred]
+        fig = plt.pie(id_score,labels=names,labeldistance=1.15)
+        fig.update_layout(title_text="Client score",title_x=0, yaxis_title=None, xaxis_title=None)
 
-#get_client(id,data_train)
-@app.get('/')
-def index():
-    col_index = len(data_train.columns)
-    return {'Welcome to this API for loan advisor'}
-
-#@app.get('/{name}')
-#def get_name(name : str):
-#    return {'Welcome ': f'{name}'}
-@app.get('/list')
-def get_list_id():
-    return data_train['SK_ID_CURR'].tolist()
-#Class which describes a single id
-class Item(BaseModel):
-    id: float
-
-@app.post('/predict')
-def predict_bank(item : Item):
-    data = item.dict()
-    #col_index = len(data_train.columns)
-    row_index = data_train['SK_ID_CURR'].tolist().index(data['id'])
-    column_value_id = data_train.iloc[row_index][2:]
-    #row_index =np.where(data_train['SK_ID_CURR'] == data['id'])
-    #column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
-    df_column_value_id = column_value_id.to_frame().T
-    prediction=pipeline_process.predict_proba(df_column_value_id)[:,1]
-    id_score = str(prediction[0])
-    print(id_score)
-    return id_score 
-
-#@app.get('/predict/{id}')
-#def predict_note(id):
-
-    col_index = len(data_train.columns)
-    #row_index =np.where(data_train['SK_ID_CURR'] == id)
-    #column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
-    #df_column_value_id = column_value_id.to_frame().T
-    #prediction=pipeline_process.predict_proba(df_column_value_id)[:,1]
-    #print(prediction)
-    #return prediction
-"""
-@app.get('/')
-def get_name():
-    col_index = len(data_train.columns)
-    return {'Welcome': len(data_train.columns)}
-
-
-# 2. Define the default route 
-@app.get("/")
-def root():
-    return {"message": "Welcome to Your Sentiment Classification FastAPI"}
-
-
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
-@app.post('/predict/')
-def predict_note(id):
-
-    col_index = len(data_train.columns)
-    row_index =np.where(data_train['SK_ID_CURR'] == id)
-    column_value_id = data_train.iloc[row_index[0][0], 1:col_index]
-    df_column_value_id = column_value_id.to_frame().T
-    prediction=pipeline_process.predict_proba(df_column_value_id)[:,1]
-    print(prediction)
-    return prediction
-
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-
-"""
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
-
-#uvicorn app:app --reload
+if __name__=='__main__':
+    main()
